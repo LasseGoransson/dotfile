c757374c187d269899047ae9d99b1e281c424096
lasseg <lasse973@gmail.com>
2019-09-27 08:50:40 +0200

    lockscreen

--- scripts/lockscreenoff.sh
+++ scripts/lockscreenoff.sh
@@ -1,4 +1,4 @@
 CURRENT="$(xbacklight)"
-xbacklight -set 0 -time 2000 -steps 100 && xset dpms force off
+xbacklight -set 0 -time 1000 -steps 100 && xset dpms force off
 i3lock  -c 000001 &
 xbacklight -set $CURRENT
